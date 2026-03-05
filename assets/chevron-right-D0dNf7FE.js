fcf122081a44d72b8e395cc589f4d38cc91533a6
fbmxdev <fbmxdev@gmail.com>
2026-03-05 11:58:48 +0800

    Updates

---
+++ assets/chevron-right-D0dNf7FE.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-B9yW5VOu.js";import"./index-DH1R4BPy.js";import"./index.es-Bqjqv1Wp.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as chevronRightSvg};
