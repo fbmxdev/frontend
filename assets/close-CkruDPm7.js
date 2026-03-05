9a1e33d03bf7965ffc69ef2d3f8ccf4a3a061fa0
fbmxdev <fbmxdev@gmail.com>
2026-03-05 12:07:53 +0800

    Updates

--- assets/close-CkruDPm7.js
+++
@@ -1,8 +0,0 @@
-import{w as l}from"./core-B9yW5VOu.js";import"./index-DH1R4BPy.js";import"./index.es-Bqjqv1Wp.js";const i=l`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as closeSvg};
