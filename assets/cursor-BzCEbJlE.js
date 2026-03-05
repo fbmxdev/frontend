9f179facbba2cbe2f53e185f204f1c5ef2736f8c
fbmxdev <fbmxdev@gmail.com>
2026-03-05 12:23:50 +0800

    Updates

--- assets/cursor-BzCEbJlE.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-B9yW5VOu.js";import"./index-DH1R4BPy.js";import"./index.es-Bqjqv1Wp.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
