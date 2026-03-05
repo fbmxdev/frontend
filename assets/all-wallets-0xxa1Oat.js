23d65cc9455d55dc48da8dbd75dad85f854bfcaf
fbmxdev <fbmxdev@gmail.com>
2026-03-05 12:27:31 +0800

    Updates

--- assets/all-wallets-0xxa1Oat.js
+++
@@ -1,6 +0,0 @@
-import{w as l}from"./core-DYr4J0_y.js";import"./index-DpzEUne8.js";import"./index.es-BF1-iyk4.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};
